1b7bdda7644b046d7ff6c3dd703aeb67885ac87c
Kacper Duras <contact@programmingwizzard.com>
2017-01-24 16:12:13 +0100

    init

---
+++ src/main/java/com/programmingwizzard/channelmanager/ChannelTask.java
@@ -0,0 +1,24 @@
+package com.programmingwizzard.channelmanager;
+
+/*
+ * @author ProgrammingWizzard
+ * @date 22.01.2017
+ */
+public class ChannelTask extends Thread
+{
+    private final Main main;
+
+    public ChannelTask(Main main)
+    {
+        this.main = main;
+    }
+
+    @Override
+    public void run()
+    {
+        while (true)
+        {
+
+        }
+    }
+}
